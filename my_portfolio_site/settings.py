754afee9aa930ed0d990a0f9bde56ce98c6368f8
tanakashouta000-ops <tanakashouta000@example.com>
2025-11-04 19:17:16 +0900

    fix Procfile and settings

--- my_portfolio_site/settings.py
+++ my_portfolio_site/settings.py
@@ -63,10 +63,10 @@ TIME_ZONE = 'Asia/Tokyo'
 USE_I18N = True
 USE_TZ = True
 
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
-STATIC_ROOT = BASE_DIR / 'staticfiles' 
+MEDIA_ROOT = BASE_DIR / 'media' 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
